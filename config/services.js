a63160a2e89e189f7a3c56b531ea52b150f11a16
Thauan <thaubr28@gmail.com>
2020-01-01 19:47:59 -0300

    Implementation social authprovider

---
+++ config/services.js
@@ -0,0 +1,12 @@
+const Env = use("Env")
+
+module.exports = {
+  ally: {
+    // Configuration for github
+    github: {
+      clientId: Env.get("GITHUB_CLIENT_ID"),
+      clientSecret: Env.get("GITHUB_CLIENT_SECRET"),
+      redirectUri: `${Env.get("APP_URL")}/authenticated/github`
+    }
+  }
+}
--- start/routes.js
+++ start/routes.js
@@ -5,3 +5,6 @@ const Route = use("Route")
 
 Route.post("/users", "UserController.create")
 Route.post("/auth", "SessionController.create")
+
+Route.get("auth/:provider", "AuthController.redirectToProvider")
+Route.get("github/callback", "AuthController.callback")
